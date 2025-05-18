aed1b5e7e5170984ad9307c366e8df0b23666b1d
Niek Beijloos <Niek.Beijloos@sioux.eu>
2025-05-18 15:07:55 +0200

    Refactored python infrastructure for ca certificate generation

--- src/infrastructure/create_python_venv.js
+++ src/infrastructure/create_python_venv.js
@@ -9,8 +9,8 @@ case 'win32':
   spawnSync('venv\\Scripts\\python', ['-m', 'pip', 'install', '-r', ...args], { stdio: 'inherit' });
   break;
 case 'linux':
-  spawnSync('sudo', ['apt', 'install', '-y', 'python3-venv'], { stdio: 'inherit' });
-  spawnSync('python3', ['-m', 'venv', 'venv'], { stdio: 'inherit' });
+  spawnSync('sudo', ['apt', 'install', '-y', 'python3.9-venv'], { stdio: 'inherit' });
+  spawnSync('python3.9', ['-m', 'venv', 'venv'], { stdio: 'inherit' });
   spawnSync('venv/bin/python', ['-m', 'pip', 'install', '-r', ...args], { stdio: 'inherit' });
   break;
 }
